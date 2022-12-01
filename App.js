22de3a3621bb7a0ad5bc5bb1b174e564d05e3aca
CurtisMacLean <csjmaclean@gmail.com>
2022-11-30 20:22:47 -0700

    have the fetch post request to add an item.  The data will have to be added dynamically to work propperly

--- App.js
+++ App.js
@@ -9,17 +9,44 @@ export default function App() {
       'http://34.27.133.88:8080/api/items'
     );
     let json = await response.json();
+    console.log("--------------------" + JSON.stringify({
+      "id": 1, 
+      "name": "aaaaa", 
+      "price": "5.00", 
+      "quantity": 1, 
+      "supplier_id": 50001
+    }) + "---------------------");
     console.log(json);
     return json;
   };
 
   const addItemTOApi = async () => {
+    try{
     let response = await fetch(
-      'http://34.27.133.88:8080/api/items'
-    );
+      'http://34.27.133.88:8080/api/items', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+
+  // fill out the body with the data you want to send
+  // resending this again will give an error since it already exists
+  body: JSON.stringify({
+    "id": 11, 
+    "name": "aaaaa", 
+    "price": "5.00", 
+    "quantity": 1, 
+    "supplier_id": 50001
+  })
+});
     let json = await response.json();
-    console.log(json);
-    return json;
+    //console.log(json);
+    
+  } catch (error) {
+    console.error("--------------" + error);
+ }
+    //return json;
   };
 
 
@@ -27,8 +54,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <TouchableOpacity onPress={getItemsFromApi}>
-          <Text>This is a button</Text>
+      <TouchableOpacity onPress={addItemTOApi}>
+          <Text style={styles.text}>Add Item to List</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={getItemsFromApi}>
+          <Text style={styles.text}>Call list</Text>
         </TouchableOpacity>
     </View>
   );
@@ -41,4 +71,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+  fontSize: 40,
+  
+  }
 });
