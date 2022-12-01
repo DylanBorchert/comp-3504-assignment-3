d32bb16585c1991747155c3e7cc3a0e27c1e6c37
CurtisMacLean <csjmaclean@gmail.com>
2022-11-30 18:39:15 -0700

    got the fetch working and am posting the data to the console

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const getArticlesFromApi = async () => {
+    let response = await fetch(
+      'http://34.27.133.88:8080/api/items'
+    );
+    let json = await response.json();
+    console.log(json);
+    return json;
+  };
+
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={getArticlesFromApi}>
+          <Text>This is a button</Text>
+        </TouchableOpacity>
     </View>
   );
 }
