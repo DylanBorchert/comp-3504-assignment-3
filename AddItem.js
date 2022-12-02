b056aefe684e48d9a1bfffcbc226716f5b010fde
Anthony Dang <anthonydang230@gmail.com>
2022-12-01 22:48:47 -0700

    Created basic AddItem page and functionality

---
+++ AddItem.js
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
+import { useState } from 'react';
+
+
+const AddItem = ({ }) => {
+
+    const [itemID, setItemID] = useState("");
+    const [itemName, setItemName] = useState("");
+    const [itemQuantity, setItemQuantity] = useState(0);
+    const [itemPrice, setItemPrice] = useState("");
+    const [itemSupplierID, setSupplierID] = useState(0);
+
+    const [supplierIDList, setSupplierIDList] = useState([]);
+
+    const addItemTOApi = async () => {
+        try {
+            let response = await fetch(
+                'http://34.27.133.88:8080/api/items', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "id": itemID,
+                    "name": itemName,
+                    "price": itemPrice,
+                    "quantity": itemQuantity,
+                    "supplier_id": itemSupplierID
+                })
+            });
+            let json = await response.json();
+            if (response.status == 200) {
+                console.log("item was successfully added");
+            }
+        } catch (error) {
+            console.error("--------------" + error);
+        }
+    };
+
+    return (
+
+        <View>
+            <Text>Add New Item:</Text>
+            <View>
+                <TextInput placeholder="ID" onChangeText={newText => setItemID(newText)}></TextInput>
+                <TextInput placeholder="Name" onChangeText={newText => setItemName(newText)}></TextInput>
+                <TextInput placeholder="Quantity" onChangeText={newText => setItemQuantity(newText)}></TextInput>
+                <TextInput placeholder="Price" onChangeText={newText => setItemPrice(newText)}></TextInput>
+                <SelectList></SelectList>
+            </View>
+            <View>
+                <Text>Item ID is: {itemID}</Text>
+                <Text>Item Name is: {itemName}</Text>
+                <Text>Item Quantity is: {itemQuantity}</Text>
+                <Text>Item Price is: {itemPrice}</Text>
+            </View>
+        </View>
+
+    );
+
+}
+
+export default AddItem;
--- ListItems.js
+++ ListItems.js
@@ -3,14 +3,21 @@ import { View, Text, TouchableOpacity} from "react-native";
 
 const ListItems = ({navigation}) => {
 
-    const pressHandler = () => {
+    const pressHandlerSearch = () => {
         navigation.navigate('searchforitem');
+      }
+      const pressHandlerAdd = () => {
+        navigation.navigate('additem');
       }
 
     return (
         <View>
             <TouchableOpacity>
-                <Text onPress={pressHandler}> Search for Item</Text>
+                <Text onPress={pressHandlerSearch}> Search for Item</Text>
+            </TouchableOpacity>
+            <Text>{"\n"}</Text>
+            <TouchableOpacity>
+                <Text onPress={pressHandlerAdd}> Add Item</Text>
             </TouchableOpacity>
         </View>
     )
