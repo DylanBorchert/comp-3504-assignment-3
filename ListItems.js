801ef29385607a2ee571ed3f885c28e1383d7b5a
halna399 <halna399@mtroyal.ca>
2022-12-01 21:09:43 -0700

    got the click event to work woo

--- ListItems.js
+++ ListItems.js
@@ -1,4 +1,6 @@
 import { ScrollView, FlatList, View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { Alert, Modal, Pressable, } from "react-native";
+
 import React, { useState, useEffect } from 'react';
 
 const ListItems = ({navigation}) => {
@@ -9,7 +11,10 @@ const ListItems = ({navigation}) => {
 
     //   let items = [];
       let [items, setItems] = useState([]);
-  
+      const [modalVisible, setModalVisible] = useState(false);
+      const [itemName, setItemName] = useState();
+      const [clickedItem, setClickedItem] = useState({});
+
       useEffect(() => {
          
         fetch("http://34.27.133.88:8080/api/items")
@@ -22,29 +27,67 @@ const ListItems = ({navigation}) => {
 
       }, []);
 
+    
+
+    const modelInfo = () => {
+        console.log(clickedItem)
+        return(
+            <View>
+                <Text style={styles.modalText}>{clickedItem.name}</Text>
+                <Text style={styles.modalText}>{clickedItem.price}</Text>
+                <Text style={styles.modalText}>{clickedItem.quantity}</Text>
+                <Text style={styles.modalText}>{clickedItem.supplier_id}</Text>
+            </View>
+        )
+    }
+
+// console.log(itemName)
 
     return (
-       
-        <View>
+        <View style={styles.container} >
             <Text style={styles.header}>List of Items</Text>
-            <FlatList
-                style={styles.container}
-                data={items}
-                keyExtractor={item => item.first}
-                renderItem={({ item }) => (
-                    
-                    <View style={styles.box}>
-                        <Text style={styles.username}>{item.name}</Text>
+            <View style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                    Alert.alert("Modal has been closed.");
+                    setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Item Name: {clickedItem.name}</Text>
+                        <Text style={styles.modalText}>Item Price: {clickedItem.price}</Text>
+                        <Text style={styles.modalText}>Item Quantity: {clickedItem.quantity}</Text>
+                        <Text style={styles.modalText}>Item Supplier: {clickedItem.supplier_id}</Text>
+                        <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => setModalVisible(!modalVisible)}
+                        >
+                        <Text style={styles.textStyle}>Hide Modal</Text>
+                        </Pressable>
                     </View>
-                )}
-                />
-            
+                    </View>
+                </Modal>
+                <FlatList
+                    data={items}
+                    keyExtractor={item => item.first}
+                    renderItem={({ item }) => ( 
+                        <Pressable style={[styles.box]} onPress={() => {setModalVisible(true); setItemName(item.name); setClickedItem(item)}} >
+                            <View>
+                                <Text style={styles.username}>{item.name}</Text>
+                            
+                            </View>
+                    </Pressable>    
+                    )}
+                    />
+            </View>
             <TouchableOpacity>
                 <Text onPress={pressHandler} style={styles.button}> Search for Item</Text>
-            </TouchableOpacity>
-        
+            </TouchableOpacity> 
         </View>
-    
     );
 
 }
@@ -52,8 +95,11 @@ const ListItems = ({navigation}) => {
 
 const styles = StyleSheet.create ({
     container: {
-        height:420
+        height:700
       }, 
+      buttonClose: {
+        backgroundColor: "#2196F3",
+      },
       username:{
         color: "284b63",
         fontSize:22,
@@ -69,12 +115,13 @@ const styles = StyleSheet.create ({
       },
       
   box: {
-    padding:5,
+    width: 350,
+    padding:10,
     marginTop:5,
     marginBottom:5,
     backgroundColor: '#FFFFFF',
     flexDirection: 'row',
-    borderRadius: 15,
+    borderRadius: 10,
     shadowColor: 'black',
     shadowOpacity: .2,
     shadowOffset: {
@@ -90,8 +137,11 @@ const styles = StyleSheet.create ({
     alignSelf:'center',
     marginLeft:10,
     padding: 10,
+    // borderWidth: 5, 
+    // // borderColor:'#FFFFFF',
+    // borderRadius: 10,
     backgroundColor: '#FFFFFF',
-    borderRadius:30,
+    borderRadius: 10,
     shadowColor: 'black',
     shadowOpacity: .2,
     shadowOffset: {
@@ -100,7 +150,40 @@ const styles = StyleSheet.create ({
     },
     elevation:2
 
+  }, 
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
+
+
+
 })
 
 export default ListItems;
